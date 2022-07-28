63492dd9da9a4d7433545bb38f7eabf6ba3b38a5
Irene Njeru <irenenjeru8@gmail.com>
2022-07-28 17:02:36 +0300

    task completed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+def show
+    user = User.find_by(id: session[:user_id])
+
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized"}, status: :unauthorize
+    end
+  end
+end
